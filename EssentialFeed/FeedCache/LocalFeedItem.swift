f9fc1231abb8fd592b1ac97a0a9b9165cae348a2
Gideon Benz <gideonbenz@gmail.com>
2022-03-01 16:41:22 +0700

    move `LocalFeedItem` to its own file

--- EssentialFeed/FeedCache/LocalFeedItem.swift
+++ EssentialFeed/FeedCache/LocalFeedItem.swift
@@ -5,4 +5,16 @@
 //  Created by Gideon Benz on 01/03/22.
 //
 
-import Foundation
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
