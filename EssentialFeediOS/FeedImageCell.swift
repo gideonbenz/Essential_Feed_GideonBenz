b7d956fbf5c4056160dfd48408a47564543055fd
Gideon Benz <gideonbenz@gmail.com>
2022-04-18 16:21:12 +0700

    render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Gideon Benz on 18/04/22.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
