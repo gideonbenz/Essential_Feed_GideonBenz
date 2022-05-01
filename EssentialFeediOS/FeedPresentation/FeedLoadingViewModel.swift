29282b6669ca05a9b7235ecb657d601982c5fdf2
Gideon Benz <gideonbenz@gmail.com>
2022-05-01 14:20:17 +0700

    move ViewModels to separate files

---
+++ EssentialFeediOS/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Gideon Benz on 01/05/22.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
