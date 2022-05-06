86aecb61e34ab7e9175a2016877b83ebc256bb9b
Gideon Benz <gideonbenz@gmail.com>
2022-05-06 15:24:30 +0700

    move `FeedImageViewModel` to separate file

--- EssentialFeed/FeedPresentation/FeedImagePresenter.swift
+++ EssentialFeed/FeedPresentation/FeedImagePresenter.swift
@@ -5,18 +5,6 @@
 //  Created by Gideon Benz on 06/05/22.
 //
 
-public struct FeedImageViewModel<Image> {
-    public let description: String?
-    public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
-    
-    public var hasLocation: Bool {
-        return location != nil
-    }
-}
-
 public protocol FeedImageView {
     associatedtype Image
     
