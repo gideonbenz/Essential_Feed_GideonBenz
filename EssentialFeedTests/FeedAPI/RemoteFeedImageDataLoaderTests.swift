5adf0b378053937a53e558a4dede6273c049deb2
Gideon Benz <gideonbenz@gmail.com>
2022-05-07 18:50:50 +0700

    `RemoteFeedImageDataLoader` delivers error on client error

--- EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
@@ -15,8 +15,13 @@ private final class RemoteFeedImageDataLoader {
         self.client = client
     }
     
-    func loadImageData(from url: URL) {
-        client.get(from: url) { _ in }
+    func loadImageData(from url: URL, completion: @escaping ((FeedImageDataLoader.Result) -> Void)) {
+        client.get(from: url) { result in
+            switch result {
+            case let .failure(error): completion(.failure(error))
+            default: break
+            }
+        }
     }
 }
 
@@ -32,7 +37,7 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT()
         
-        sut.loadImageData(from: url)
+        sut.loadImageData(from: url) { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
     }
@@ -41,10 +46,27 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT()
         
-        sut.loadImageData(from: url)
-        sut.loadImageData(from: url)
+        sut.loadImageData(from: url) { _ in }
+        sut.loadImageData(from: url) { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT()
+        let clientError = NSError(domain: "a client error", code: 0)
+        
+        let exp = expectation(description: "Wait for load completion")
+        sut.loadImageData(from: url) { result in
+            if case let .failure(receivedError as NSError) = result {
+                XCTAssertEqual(receivedError, clientError)
+                exp.fulfill()
+            }
+        }
+        client.complete(with: clientError)
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     // MARK: - Helpers
@@ -58,10 +80,16 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
     }
     
     private final class HTTPClientSpy: HTTPClient {
+        private var messages = [(url: URL, completion: ((HTTPClient.Result) -> Void))]()
         var requestedURLs = [URL]()
-        
+         
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             requestedURLs.append(url)
+            messages.append((url: url, completion: completion))
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
         }
     }
 }
