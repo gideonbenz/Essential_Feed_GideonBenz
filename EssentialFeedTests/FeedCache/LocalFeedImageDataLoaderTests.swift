c5054b1d020ea319d814ca743bc81e851f7d62d1
Gideon Benz <gideonbenz@gmail.com>
2022-05-15 12:21:41 +0700

    `LocalFeedImageDataLoader.loadImageData` requests stored data for URL from `FeedImageDataStore`

--- EssentialFeedTests/FeedCache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/FeedCache/LocalFeedImageDataLoaderTests.swift
@@ -7,9 +7,19 @@
 
 import XCTest
 
+protocol FeedImageDataStore {
+    func retrieve(dataForURL url: URL)
+}
+
 final class LocalFeedImageDataLoader {
-    init(store: Any) {
-        
+    private let store: FeedImageDataStore
+    
+    init(store: FeedImageDataStore) {
+        self.store = store
+    }
+    
+    func loadImageData(from url: URL) {
+        store.retrieve(dataForURL: url)
     }
 }
 
@@ -21,17 +31,30 @@ final class LocalFeedImageDataLoaderTests: XCTestCase {
         XCTAssertTrue(store.receivedMessage.isEmpty)
     }
     
+    func test_loadImageDataFromURL_requestsStoreDataForURL() {
+        let (sut, store) = makeSUT()
+        let url = anyURL()
+        
+        sut.loadImageData(from: url)
+        
+        XCTAssertEqual(store.receivedMessage, [url])
+    }
+    
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
-        let store = FeedStoreSpy()
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
+        let store = StoreSpy()
         let sut = LocalFeedImageDataLoader(store: store)
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
     
-    private final class FeedStoreSpy {
-        let receivedMessage = [Any]()
+    private final class StoreSpy: FeedImageDataStore {
+        var receivedMessage = [URL]()
+        
+        func retrieve(dataForURL url: URL) {
+            receivedMessage.append(url)
+        }
     }
 }
